02e3473bcd6b1f95bd2c514d33406535766bdb79
noa <noa@localhost>
2025-08-02 23:30:20 +0900

    1-30 再提出

--- HelloWorld_YanoMiki/src/self_introduction_1/Main.java
+++ HelloWorld_YanoMiki/src/self_introduction_1/Main.java
@@ -4,7 +4,7 @@ public class Main {
 
 	public static void main(String[] args) {
 		// TODO 自動生成されたメソッド・スタブ
-		Person person1 = new Person("鈴木太郎",20,1.7,60);
+		Person person1 = new Person("鈴木太郎",20,1.7,57.8);
 		
 		System.out.println(person1.name);
 	    System.out.println(person1.age);
