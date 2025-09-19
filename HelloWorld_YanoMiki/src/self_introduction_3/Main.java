a1fc062d11e9cc5c11488a27bc1652c0e78c8260
noa <noa@localhost>
2025-09-19 23:24:10 +0900

    2.33 新規作成

---
+++ HelloWorld_YanoMiki/src/self_introduction_3/Main.java
@@ -0,0 +1,18 @@
+package self_introduction_3;
+
+public class Main {
+
+	public static void main(String[] args) {
+		// TODO 自動生成されたメソッド・スタブ
+		
+		Person person1 = new Person("太郎", "鈴木", 20, 1.7, 60);
+		person1.print();
+
+		Person person2 = new Person("花子", "山田", 22, 1.5, 40);
+		person2.print();
+
+		Person.printCount();
+
+	}
+
+}
---
+++ HelloWorld_YanoMiki/src/self_introduction_3/Person.java
@@ -0,0 +1,47 @@
+package self_introduction_3;
+
+public class Person {
+
+	 // インスタンスフィールド
+	 private static int count = 0;
+	 private String firstName;
+	 private String lastName;
+	 private int age;
+	 private double height, weight;
+	 
+	 // コンストラクタ
+	 Person(String firstName, int age, double height, double weight) {
+	  this.firstName = firstName;
+	  this.age = age;
+	  this.height = height;
+	  this.weight = weight;
+	  count++;
+	 }
+	 
+	 // 追加コンストラクタ
+	 Person(String firstName, String lastName, int age, double height, double weight){
+	  this.firstName = firstName;
+	  this.lastName = lastName;
+	  this.age = age;
+	  this.height = height;
+	  this.weight = weight;
+	  Person.count++;
+	 }
+
+	 public String fullName(){
+	  return this.firstName + this.lastName;
+	 }
+
+	 public void print(){
+	  System.out.println("名前は" + this.fullName() + "です");
+	  System.out.println("年は" + this.age + "です");
+	 }
+
+	 public double bmi(){
+	  return this.weight / this.height / this.height;
+	 }
+
+	 public static void printCount(){
+	  System.out.println("合計" + Person.count + "人です");
+	 }
+	}
